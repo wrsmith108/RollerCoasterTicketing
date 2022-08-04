d8d01767c8ef3652ce755686cd240ce14169dffc
Ryan Smith <62075823+wrsmith108@users.noreply.github.com>
2022-08-04 22:18:19 +0000

    height and age check for ticket

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height < 150:
+  print("Sorry, you are not tall enough for this ride.")
+else:
+  print("Welcome! Please buy a ticket from the agent.")
+  age = int(input("What is your age? "))
+  if age < 12:
+    print("Please pay $5")
+  elif age <= 18:
+    print("Please by $7")
+  else:
+    print("Please pay $12")
